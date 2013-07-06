fa9f7705350ef6db4bc403ea41a7508263a6f977
Dahua Lin <lindahua@gmail.com>
2013-07-05 21:31:16 -0500

    minor fix to logisticreg

--- src/logisticreg.jl
+++ src/logisticreg.jl
@@ -70,7 +70,7 @@ end
 #
 ####
 
-function logisticreg_objfun(x::Matrix{Float64}, y::Vector{Float64}, r::Float64; 
+function logisticreg_objfun(x::Matrix{Float64}, y::Vector{Float64}, r::Regularizer; 
 	by_columns::Bool=false, bias::Bool=false)
 
 	generic_regress_objfun(LogisticRegressFunctor(), x, y, 1.0; by_columns=by_columns, bias=bias)
