9fecd828f13ce27a62070d7adaf4cdfcaee48e37
Dahua Lin <lindahua@gmail.com>
2013-07-05 22:27:36 -0500

    add inbounds macro to speed up logistic regression

--- src/logisticreg.jl
+++ src/logisticreg.jl
@@ -18,8 +18,8 @@ function evaluate_values!(
 	v::Vector{Float64})
 
 	for i in 1 : length(u)
-		x = y[i] * u[i]
-		v[i] = x > 0 ? log1p(exp(-x)) : -x + log1p(exp(x))
+		@inbounds x = y[i] * u[i]
+		@inbounds v[i] = x > 0 ? log1p(exp(-x)) : -x + log1p(exp(x))
 	end
 end
 
@@ -30,13 +30,13 @@ function evaluate_derivs!(
 	g::Vector{Float64})
 
 	for i in 1 : length(u)
-		yi = y[i]
-		x = yi * u[i]
+		@inbounds yi = y[i]
+		@inbounds x = yi * u[i]
 		if x > 0
 			t = exp(-x)
-			g[i] = - yi * t / (1 + t)
+			@inbounds g[i] = - yi * t / (1 + t)
 		else
-			g[i] = - yi / (1 + exp(x))
+			@inbounds g[i] = - yi / (1 + exp(x))
 		end
 	end
 end
@@ -49,16 +49,16 @@ function evaluate_values_and_derivs!(
 	g::Vector{Float64})
 
 	for i in 1 : length(u)
-		yi = y[i]
-		x = yi * u[i]
+		@inbounds yi = y[i]
+		@inbounds x = yi * u[i]
 		if x > 0
 			t = exp(-x)
-			v[i] = log1p(t)
-			g[i] = - yi * t / (1 + t)
+			@inbounds v[i] = log1p(t)
+			@inbounds g[i] = - yi * t / (1 + t)
 		else
 			t = exp(x)
-			v[i] = -x + log1p(t)
-			g[i] = - yi / (1 + t)
+			@inbounds v[i] = -x + log1p(t)
+			@inbounds g[i] = - yi / (1 + t)
 		end
 	end
 end
